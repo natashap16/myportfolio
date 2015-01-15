0b0715445af123cd58fb86093ca962c9a0632089
natashap16 <ncprats@stanford.edu>
2015-01-14 19:34:23 -0500

    flipper added to project thumbnails, but most projects still missing descriptions

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,20 +4,28 @@ $( document ).ready(function() {
 	var source   = $("#project-small-template").html();
  	var template = Handlebars.compile(source);
   	var data = { projects: [
-      {url: "/projects/vincere.html", workCategory:"which-uxresearch-item which-coding-item which-uxdesign-item", imageURL: "assets/img/project_thumbnails/vincere.png", styleInfo:"position:relative; top:10px"},
-      {url:"/projects/expressway.html", workCategory:"which-uxresearch-item which-needfinding-item which-uxdesign-item", imageURL:"assets/img/project_thumbnails/Expressway.png"},
-      {url:"#", workCategory:"which-needfinding-item", imageURL:"assets/img/project_thumbnails/mr-sushi.jpg"},
-      {url:"#", workCategory:"which-needfinding-item which-uxresearch-item", imageURL:"assets/img/project_thumbnails/toadette_stool.jpg"},
-      {url:"/projects/socialize.html", workCategory:"which-coding-item which-uxdesign-item", imageURL:"assets/img/project_thumbnails/Socialize-upcoming-events.jpg"},
-      {url:"#", workCategory:"which-uxresearch-item", imageURL:"assets/img/project_thumbnails/time-lapse-altschool.jpg"},
-      {url:"#", workCategory:"which-uxresearch-item", imageURL:"assets/img/project_thumbnails/usability-testing-google.jpg"},
-      {url:"#", workCategory:"which-uxresearch-item", imageURL:"assets/img/project_thumbnails/teacher-card-sort.png"},
-      {url:"#", workCategory:"which-uxdesign-item", imageURL:"assets/img/project_thumbnails/pen.jpg"}
+      {title:"Vincere", flipperID: "myFlipper1", url: "/projects/vincere.html", workCategory:"which-uxresearch-item which-coding-item which-uxdesign-item", imageURL: "assets/img/project_thumbnails/vincere.png", styleInfo:"position:relative; top:10px", description:"A site designed to anonymously connect survivors of sexual assault. They would connect over an audio stream and a shared drawing canvas, so they could doodle and create something together while they talk."},
+      {title:"Expressway", flipperID: "myFlipper2", url:"/projects/expressway.html", workCategory:"which-uxresearch-item which-needfinding-item which-uxdesign-item", imageURL:"assets/img/project_thumbnails/Expressway.png"},
+      {title:"Rolls of the World", flipperID:"myFlipper3", url:"#", workCategory:"which-needfinding-item", imageURL:"assets/img/project_thumbnails/mr-sushi.jpg"},
+      {title:"Toadette", flipperID:"myFlipper4", url:"#", workCategory:"which-needfinding-item which-uxresearch-item", imageURL:"assets/img/project_thumbnails/toadette_stool.jpg"},
+      {title:"Socialize", flipperID:"myFlipper5", url:"/projects/socialize.html", workCategory:"which-coding-item which-uxdesign-item", imageURL:"assets/img/project_thumbnails/Socialize-upcoming-events.jpg"},
+      {title:"Teacher Time Study at AltSchool", flipperID:"myFlipper6",url:"#", workCategory:"which-uxresearch-item", imageURL:"assets/img/project_thumbnails/time-lapse-altschool.jpg"},
+      {title:"Usability Testing at Google", flipperID:"myFlipper7",url:"#", workCategory:"which-uxresearch-item", imageURL:"assets/img/project_thumbnails/usability-testing-google.jpg"},
+      {title:"Teacher Planning Study", flipperID:"myFlipper8",url:"#", workCategory:"which-uxresearch-item", imageURL:"assets/img/project_thumbnails/teacher-card-sort.png"},
+      {title:"A Pen in the Style of Ross Lovegrove", flipperID:"myFlipper9",url:"#", workCategory:"which-uxdesign-item", imageURL:"assets/img/project_thumbnails/pen.jpg"}
     ]};
   	$("#content-placeholder").html(template(data));
 
 	//image flipper
-	$('#myFlipper').flipper();
+	$('#myFlipper1').flipper();
+	$('#myFlipper2').flipper();
+	$('#myFlipper3').flipper();
+	$('#myFlipper4').flipper();
+	$('#myFlipper5').flipper();
+	$('#myFlipper6').flipper();
+	$('#myFlipper7').flipper();
+	$('#myFlipper8').flipper();
+	$('#myFlipper9').flipper();
 
 	// Closes the sidebar menu
 	$("#menu-close").click(function(e) {
